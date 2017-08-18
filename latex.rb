b876ad28251713634a98cf69e6a49d9915cbd649
Yuncong Zhang <shjdzhangyuncong@sjtu.edu.cn>
2017-08-18 11:11:17 +0800

    Define LatexTable class. Store test data into files.

--- latex.rb
+++ latex.rb
@@ -2,50 +2,70 @@
 
 require 'csv'
 
-def latexTable(input)
-	output = ""
-	lines = input.split "\n"
-	tokens = []
-	i = 0
-	max = 0
-	CSV.parse(input) do |row|
-		tokens << row
-		if row.length > max
-			max = row.length
-		end
-		i += 1
-	end
-	if max == 0
-		return ""
-	end
-	columnWidths = []
-	tokens.each do |row|
-		0.upto(max-1) do |i|
-			if row[i] != nil
-				row[i] = row[i].strip
-				if columnWidths[i] == nil or columnWidths[i] < row[i].length
-					columnWidths[i] = row[i].length
+class LatexTable
+	def initialize(input)
+		lines = input.split "\n"
+		@tokens = []
+		@columnWidths = []
+		CSV.parse(input) do |row|
+			@tokens << row
+			row.each_with_index do |token,i|
+				if token != nil
+					token = token.strip
+					row[i] = token
+					if @columnWidths[i] == nil or @columnWidths[i] < token.length
+						@columnWidths[i] = token.length
+					end
 				end
 			end
 		end
 	end
-	output += "\\begin{table}\n"
-	output += "  \\begin{tabular}{#{"c"*max}}\n"
-	tokens.each do |row|
-		output += "    "
-		0.upto(max-1) do |i|
-			if row[i] != nil
-				output += row[i]
-				output += " "*(columnWidths[i]-row[i].length)
-			else
-				output += " "*columnWidths[i]
-			end
-			if i < max-1
-				output += " & "
-			end
-		end
-		output += "\\\\\n"
+
+	def empty?
+		return @tokens.empty?
 	end
-	output += "  \\end{tabular}\n"
-	output += "\\end{table}\n"
+
+	def width
+		return 0 if empty?
+		@tokens.last.length
+	end
+
+	def height
+		@tokens.length
+	end
+
+	def columnWidth(i)
+		return 0 if i >= @columnWidths.length or i < 0
+		@columnWidths[i]
+	end
+
+	def get(i,j)
+		return "" if i < 0 or i >= height or j < 0 or j >= width
+		return "" if @tokens[i][j] == nil
+		@tokens[i][j]
+	end
+
+	def to_s
+		return "" if empty?
+		output = "\\begin{table}\n"
+		output += "  \\begin{tabular}{#{"c"*width}}\n"
+		0.upto(height-1) do |i|
+			output += "    "
+			0.upto(width-1) do |j|
+				token = get(i,j)
+				output += token
+				output += " "*(columnWidth(j)-token.length)
+				if j < width-1
+					output += " & "
+				end
+			end
+			output += "\\\\\n"
+		end
+		output += "  \\end{tabular}\n"
+		output += "\\end{table}\n"
+	end
+end
+
+def latexTable(input)
+	LatexTable.new(input).to_s
 end
--- latex_test.rb
+++ latex_test.rb
@@ -2,11 +2,28 @@ require_relative 'latex'
 require 'test/unit'
 
 class TestLatexTable < Test::Unit::TestCase
-	def testEmptyString
-		assert_equal("",latexTable(""))
+	private
+	def testFile(inputFile,outputFile)
+		input = ""
+		output = ""
+		f = File.open("test_data/latex/#{inputFile}.txt")
+		f.each do |line|
+			input += line
+		end
+		f.close
+		f = File.open("test_data/latex/#{outputFile}.txt")
+		f.each do |line|
+			output += line
+		end
+		f.close
+		assert_equal(output,latexTable(input))
 	end
 
-	def testSingleChar
-		assert_equal("\\begin{table}\n  \\begin{tabular}{c}\n    a\\\\\n  \\end{tabular}\n\\end{table}\n",latexTable("a"))
+	public
+	def testFiles
+		testFile("table_input_1","table_output_1")
+		testFile("table_input_2","table_output_2")
+		testFile("table_input_3","table_output_3")
+		testFile("table_input_4","table_output_4")
 	end
 end
